dd967a44afc2918303a46664967811adf07ff3b1
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2019-07-16 17:06:15 -0500

    Converted to CameraX. Capture and front/back functionality working

--- app/src/main/java/com/erm/visionary/MainActivity.kt
+++ app/src/main/java/com/erm/visionary/MainActivity.kt
@@ -1,75 +1,163 @@
 package com.erm.visionary
 
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
-import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.Manifest.permission.*
+import android.content.pm.PackageManager
+import android.graphics.Matrix
 import android.os.Bundle
-import android.os.Environment
+import android.util.DisplayMetrics
+import android.util.Rational
+import android.util.Size
+import android.view.Surface
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
-import java.io.FileOutputStream
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_CODE_PERMISSIONS = 10
+    private val REQUIRED_PERMISSIONS = arrayOf(WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE, CAMERA)
+
+    private var lensFacing = CameraX.LensFacing.BACK
+    private var photoFile: File? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_capture.setOnClickListener {
-            camera?.captureImage { cameraKitView, capturedImageBytes ->
-                val photoFile = File(
-                    Environment.getExternalStoragePublicDirectory(
-                        Environment.DIRECTORY_PICTURES
-                    ), "${System.currentTimeMillis()}_visionary"
-                )
 
-                val outputStream = FileOutputStream(photoFile.path)
-                try {
-                    Glide.with(this@MainActivity).load(photoFile).into(picture)
-                    camera?.visibility = View.GONE
-                    picture.visibility = View.VISIBLE
-                    outputStream.write(capturedImageBytes)
-                } catch (e: Throwable) {
-                    e.printStackTrace()
-                } finally {
-                    outputStream.close()
-                }
-            }
+        btn_switch_camera.setOnClickListener {
+            lensFacing =
+                if (lensFacing == CameraX.LensFacing.BACK) CameraX.LensFacing.FRONT else CameraX.LensFacing.BACK
+            
+            CameraX.unbindAll()
+            startCamera()
         }
-        btn_switch_camera.setOnClickListener { camera?.toggleFacing() }
         btn_retake.setOnClickListener {
             picture.visibility = View.GONE
-            camera.visibility = View.VISIBLE
         }
+
+        ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+    }
+
+    private fun startCamera() {
+        val metrics = DisplayMetrics().also { camera.display.getRealMetrics(it) }
+        val screenSize = Size(metrics.widthPixels, metrics.heightPixels)
+        val screenAspectRatio = Rational(metrics.widthPixels, metrics.heightPixels)
+
+        val previewConfig = PreviewConfig.Builder().apply {
+            setLensFacing(lensFacing)
+            setTargetResolution(screenSize)
+            setTargetAspectRatio(screenAspectRatio)
+            setTargetRotation(windowManager.defaultDisplay.rotation)
+            setTargetRotation(camera.display.rotation)
+        }.build()
+
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = camera.parent as ViewGroup
+            parent.removeView(camera)
+            camera.surfaceTexture = it.surfaceTexture
+            parent.addView(camera, 0)
+            updateTransform()
+        }
+
+        // Create configuration object for the image capture use case
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
+            .apply {
+                setLensFacing(lensFacing)
+                setTargetAspectRatio(screenAspectRatio)
+                setTargetRotation(camera.display.rotation)
+                setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
+            }.build()
+
+        // Build the image capture use case and attach button click listener
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        btn_capture.setOnClickListener {
+
+            val file = File(
+                externalMediaDirs.first(), "visionary_${System.currentTimeMillis()}.jpg"
+            )
+
+            imageCapture.takePicture(file,
+                object : ImageCapture.OnImageSavedListener {
+                    override fun onError(
+                        error: ImageCapture.UseCaseError,
+                        message: String, exc: Throwable?
+                    ) {
+                        val msg = "Photo capture failed: $message"
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onImageSaved(file: File) {
+                        photoFile = file
+                        Glide.with(this@MainActivity).load(photoFile).into(picture)
+                        picture.visibility = View.VISIBLE
+                    }
+                })
+
+        }
+        CameraX.bindToLifecycle(this, preview, imageCapture)
+    }
+
+    private fun updateTransform() {
+        val matrix = Matrix()
+        val centerX = camera.width / 2f
+        val centerY = camera.height / 2f
+
+        val rotationDegrees = when (camera.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+        camera.setTransform(matrix)
     }
 
     override fun onStart() {
         super.onStart()
-        camera.onStart()
-        ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE), 111)
         picture.visibility = View.GONE
     }
 
     override fun onResume() {
         super.onResume()
-        camera.onResume()
         picture.visibility = View.GONE
     }
 
     override fun onPause() {
-        camera.onPause()
         super.onPause()
     }
 
     override fun onStop() {
-        camera.onStop()
         super.onStop()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        camera.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                camera.post { startCamera() }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
     }
 }
