8caf5d87a51c87dda461d467f346bd0e89b09923
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2019-08-08 21:52:09 -0500

    Reading text

--- app/src/main/java/com/erm/visionary/MainActivity.kt
+++ app/src/main/java/com/erm/visionary/MainActivity.kt
@@ -2,40 +2,51 @@ package com.erm.visionary
 
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.DialogInterface
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Environment
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.bumptech.glide.Glide
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.io.FileOutputStream
+import com.google.firebase.ml.vision.text.RecognizedLanguage
+import com.google.firebase.ml.vision.text.FirebaseVisionText
+
+
 
 
 class MainActivity : AppCompatActivity() {
+    private var photoFile: File? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn_capture.setOnClickListener {
-            camera?.captureImage { cameraKitView, capturedImageBytes ->
-                val photoFile = File(
+            camera?.captureImage { _, capturedImageBytes ->
+                photoFile = File(
                     Environment.getExternalStoragePublicDirectory(
                         Environment.DIRECTORY_PICTURES
                     ), "${System.currentTimeMillis()}_visionary"
                 )
 
-                val outputStream = FileOutputStream(photoFile.path)
+                val outputStream = FileOutputStream(photoFile?.path)
                 try {
-                    Glide.with(this@MainActivity).load(photoFile).into(picture)
-                    camera?.visibility = View.GONE
-                    picture.visibility = View.VISIBLE
                     outputStream.write(capturedImageBytes)
                 } catch (e: Throwable) {
                     e.printStackTrace()
                 } finally {
                     outputStream.close()
                 }
+
+                Glide.with(this@MainActivity).load(capturedImageBytes).into(picture)
+                camera?.visibility = View.GONE
+                picture.visibility = View.VISIBLE
             }
         }
         btn_switch_camera.setOnClickListener { camera?.toggleFacing() }
@@ -43,6 +54,54 @@ class MainActivity : AppCompatActivity() {
             picture.visibility = View.GONE
             camera.visibility = View.VISIBLE
         }
+        btn_recognize_text.setOnClickListener {
+            if (photoFile != null && photoFile?.exists() == true) {
+                val bitmap = BitmapFactory.decodeFile(photoFile!!.path)
+                val firebaseImage = FirebaseVisionImage.fromBitmap(bitmap)
+                val detector = FirebaseVision.getInstance()
+                    .onDeviceTextRecognizer
+                val result = detector.processImage(firebaseImage)
+                    .addOnSuccessListener {
+                        val resultText = it.getText()
+                        showAlert(resultText)
+                        for (block in it.getTextBlocks()) {
+                            val blockText = block.getText()
+                            val blockConfidence = block.getConfidence()
+                            val blockLanguages = block.getRecognizedLanguages()
+                            val blockCornerPoints = block.getCornerPoints()
+                            val blockFrame = block.getBoundingBox()
+                            for (line in block.getLines()) {
+                                val lineText = line.getText()
+                                val lineConfidence = line.getConfidence()
+                                val lineLanguages = line.getRecognizedLanguages()
+                                val lineCornerPoints = line.getCornerPoints()
+                                val lineFrame = line.getBoundingBox()
+                                for (element in line.getElements()) {
+                                    val elementText = element.getText()
+                                    val elementConfidence = element.getConfidence()
+                                    val elementLanguages = element.getRecognizedLanguages()
+                                    val elementCornerPoints = element.getCornerPoints()
+                                    val elementFrame = element.getBoundingBox()
+                                }
+                            }
+                        }
+                    }
+                    .addOnFailureListener {
+                        showAlert("Failed to read text, exception")
+                    }
+            }
+        }
+    }
+
+    private fun showAlert(text: String) {
+        AlertDialog.Builder(this)
+            .setTitle("Success")
+            .setMessage(text)
+            .setPositiveButton("Ok", object : DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    dialog?.dismiss()
+                }
+            }).show()
     }
 
     override fun onStart() {
@@ -55,7 +114,6 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         camera.onResume()
-        picture.visibility = View.GONE
     }
 
     override fun onPause() {
